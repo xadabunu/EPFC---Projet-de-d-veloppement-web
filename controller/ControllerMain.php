cb7be7054beb924b62293bdf265e955a8ef62643
Cedric <cedric.ribaucourt@gmail.com>
2022-12-08 17:24:00 +0100

    feat(UC_Signup) : Début php du signup

--- controller/ControllerMain.php
+++ controller/ControllerMain.php
@@ -1,29 +1,59 @@
 <?php
+require_once 'framework/View.php';
+require_once 'framework/Controller.php';
+require_once 'model/User.php';
 
 class ControllerMain extends Controller {
 
-public function index() : void
-{
+    public function index() : void
+    {
 	$this -> login();
-}
+    }
 
-public function login() : void {
+    public function login() : void {
 
-	$email = "";
-	$password = "";
-	$errors = [];
+	    $email = "";
+	    $password = "";
+	    $errors = [];
 
-	if (isset($_POST['email']) && isset($_POST['password']))
-	{
-		$email = trim($_POST['email']);
-		$password = trim($_POST['password']);
+	    if (isset($_POST['email']) && isset($_POST['password']))
+	    {
+		    $email = trim($_POST['email']);
+		    $password = trim($_POST['password']);
 
-		$errors = User::validate_login($email, $password);
-		if (empty($errors)) {
-			$this -> log_user(User::get_user_by_email($email));
-		}
-	}
+		    $errors = User::validate_login($email, $password);
+		    if (empty($errors)) {
+			    $this -> log_user(User::get_user_by_email($email));
+		    }
+	    }
 	(new View("login")) -> show(["email" => $email, "password" => $password, "errors" => $errors]);
-}
+    }
 
+    public function signup() : void {
+        $email = '';
+        $full_name = '';
+        $iban = '';
+        $password = '';
+        $password_confirm = '';
+        $errors = [];
+
+        if (isset($_POST['email']) && isset($_POST['password']) && isset($_POST['password_confirm'])
+            && isset($_POST['fullName']) && isset($_POST['iban'])) {
+
+            $email = trim($_POST['email']);
+            $full_name = trim($_POST['fullName']);
+            $iban = ($_POST['iban']);
+            $password = $_POST['password'];
+            $password_confirm = $_POST['password_confirm'];
+
+            $user = new User($email, $full_name, Tools::my_hash($password),'user', $iban);
+            $errors = User::validate_unicity($email);
+            $errors = array_merge($errors, $user->validate());
+            $errors = array_merge($errors, User::validate_passwords($password, $password_confirm));
+            if(count($errors) == 0){
+                $user->persist();
+                $this->log_user($user);
+            }
+        }
+    }
 }
--- model/User.php
+++ model/User.php
@@ -4,35 +4,100 @@ require_once "framework/Model.php";
 
 class User extends Model {
 
-public function __construct(public string $email, public string $hashed_password, public string $role, public string $iban) {}
+    public function __construct(public string $email, public String $full_name, public string $hashed_password, public string $role = 'user', public String $iban) {}
 
-public static function validate_login(string $email, string $password) : array
-{
-	$errors = [];
-	$user = User::get_user_by_email($email);
-	if ($user) {
-		if (!self::check_password($password, $user -> hashed_password)) {
-			$errors[] = "Wrong password. Please try again.";
-		}
-	} else {
-		$errors[] = "Can't find a user with the email '$email'. Please sign up.";
-	}
-	return $errors;
-}
+    public static function validate_login(string $email, string $password) : array
+    {
+        $errors = [];
+        $user = User::get_user_by_email($email);
+        if ($user) {
+            if (!self::check_password($password, $user -> hashed_password)) {
+                $errors[] = "Wrong password. Please try again.";
+            }
+        } else {
+            $errors[] = "Can't find a user with the email '$email'. Please sign up.";
+        }
+        return $errors;
+    }
 
-private static function check_password(string $clear_password, string $hash) : bool {
-	return $hash === Tools::my_hash($clear_password);
-}
+    private static function check_password(string $clear_password, string $hash) : bool {
+        return $hash === Tools::my_hash($clear_password);
+    }
 
-public static function get_user_by_email(string $email) : User | false
-{
-	$query = self::execute("SELECT * FROM users WHERE mail = :email", ["email" => $email]);
-	$data = $query -> fetch();
-	if ($query -> rowCount() == 0) {
-		return false;
-	} else {
-		return new User($data['email'], $data['hashed_password'], $data['full_name'], $data['role'], $data['iban']);
-	}
-}
+    public static function get_user_by_email(string $email) : User | false
+    {
+        $query = self::execute("SELECT * FROM users WHERE mail = :email", ["email" => $email]);
+        $data = $query -> fetch();
+        if ($query -> rowCount() == 0) {
+            return false;
+        } else {
+            return new User($data['email'], $data['hashed_password'], $data['full_name'], $data['role'], $data['iban']);
+        }
+    }
+
+    public static function validate_unicity(string $email) : array {
+        $errors = [];
+        $user = self::get_user_by_email($email);
+        if ($user) {
+            $errors[] = "This user already exists.";
+        }
+        return $errors;
+    }
+
+    public function validate() : array{
+        $errors = [];
+        if(!strlen($this->email) >0){
+            $errors[] = "Email is required.";
+        }
+        if(!preg_match("[a-zA-Z0-9]{1,20}[@][a-zA-A0-9]{1,15}[.][a-z]{1,7}]",$this->email)){
+            $errors [] = "Not a valid email address";
+        }
+        if(!(strlen($this->full_name) >= 3)){
+            $errors[] = "Pseudo length must be higher than 3.";
+        }
+        if(!preg_match("/^[a-zA-Z][a-zA-Z0]*$/", $this->full_name)){
+            $errors [] = "Name must contains only letters";
+        }
+        if(!preg_match("[A-Z]{2}[0-9]{2}\s[0-9]{4}\s[0-9]{4}\s[0-9]{4}]", $this->iban)){
+            $errors[] = "IBAN must have an official IBAN format";
+        }
+
+
+        return $errors;
+    }
+
+    private static function validate_password(string $password) : array {
+        $errors = [];
+        if (strlen($password) < 8 || strlen($password) > 16) {
+            $errors[] = "Password length must be between 8 and 16.";
+        } if (!((preg_match("/[A-Z]/", $password)) && preg_match("/\d/", $password) && preg_match("/['\";:,.\/?!\\-]/", $password))) {
+            $errors[] = "Password must contain one uppercase letter, one number and one punctuation mark.";
+        }
+        return $errors;
+    }
+
+    public static function validate_passwords(string $password, string $password_confirm) : array {
+        $errors = self::validate_password($password);
+        if ($password != $password_confirm) {
+            $errors[] = "You have to enter twice the same password.";
+        }
+        return $errors;
+    }
+
+
+    public function persist() : User {
+        if(self::get_user_by_email($this->email)){
+            self::execute("UPDATE users SET hashed_password=:password, full_name=:full_name, role=:role, iban=:iban WHERE email=:email",
+                 ["password"=>$this->hashed_password, "full_name"=>$this->full_name, "role"=>$this->role, "iban"=>$this->iban]);
+        }
+        else{
+            self::execute("INSERT INTO users(email, hashed_password, full_name, role, iban) VALUES(:email, :hashed_password, :full_name, :role, :iban)",
+                ["email" =>$this->email, "password"=>$this->hashed_password, "full_name"=>$this->full_name, "role"=>$this->role, "iban"=>$this->iban]);
+        }
+        return $this;
+    }
+
+
+
 
 }
