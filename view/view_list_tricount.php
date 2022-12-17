22afe4649c0ecf6ab38a0a3030cff04697f8fb46
Xavier <2907xaadabunu@student.epfc.eu>
2022-12-17 23:36:51 +0100

    build : début du css

--- view/view_list_tricount.php
+++ view/view_list_tricount.php
@@ -4,31 +4,38 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="css/styles.css" rel="stylesheet" type="text/css" />
     <base href="<?= $web_root ?>">
+    <link href="css/styles.css" rel="stylesheet" type="text/css" />
     <title>Your Tricounts</title>
 </head>
 
 <body>
-    <div class="title">Your Tricounts</div>
-    <ul>
-        <?php foreach ($data as $tricount) { ?>
-            <li class="tricount">
-                <div>
-                    <a href="tricount/operations/<?= $tricount->id ?>"><p class="title"><?= $tricount->title ?></p></a>
-                    <?php
-                    $id = $tricount->id;
-                    $n = $subs_number[$id];
-                    if ($n > 0) { ?>
-                        <p class="participants_number">
-                            <?php echo "with $n friend(s)" ?></p>
-                    <?php } ?>
-                    <p><?= $tricount->description ?></p>
-                </div>
-            </li>
-        <?php } ?>
-    </ul>
-    <a href="main/logout">Logout</a>
+    <div class="main">
+        <div class="title" id="t2">Your Tricounts</div>
+        <ul>
+            <?php foreach ($data as $tricount) { ?>
+                <li class="tricount">
+                    <div>
+                        <p class="name">
+                            <a href="tricount/operations/<?= $tricount->id ?>">
+                                <?= $tricount->title ?>
+                            </a>
+                        </p>
+                        <?php
+                        $id = $tricount->id;
+                        $n = $subs_number[$id];
+                        if ($n > 0) { ?>
+                            <p class="participants_number">
+                                <?php echo "with $n friend(s)" ?></p>
+                        <?php } ?>
+                        <p><?= $tricount->description ?></p>
+                    </div>
+                </li>
+            <?php } ?>
+        </ul>
+        <a href="main/logout">Logout</a>
+    </div>
+
 </body>
 
 </html>
--- view/view_operations.php
+++ view/view_operations.php
@@ -1,17 +1,23 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <base href="<?= $web_root ?>">
-	<link href="css/styles.css" rel="stylesheet" type="text/css" />
+    <link href="css/styles.css" rel="stylesheet" type="text/css" />
     <title>Operations</title>
 </head>
+
 <body>
-    <ul>
-        <?php foreach ($list as $operation) { ?>
-            <li> <?=$operation->title?></li>
-        <?php } ?>
-    </ul>
+    <div class="main">
+        <ul>
+            <?php foreach ($list as $operation) { ?>
+                <li> <?= $operation->title ?></li>
+            <?php } ?>
+        </ul>
+    </div>
+
 </body>
+
 </html>
