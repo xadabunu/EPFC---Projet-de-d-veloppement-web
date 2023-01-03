119aadedecae5c3807d9e4408341f82fad487c78
Xavier <2907xaadabunu@student.epfc.eu>
2023-01-03 10:35:07 +0100

    feat(CSS): CSS partiel de la view_edit_tricount

--- view/view_edit_tricount.php
+++ view/view_edit_tricount.php
@@ -3,7 +3,7 @@
 
 <head>
     <meta charset="utf8">
-    <title>Edit_Tricount</title>
+    <title><?= $tricount->title ?> &#11208; Edit</title>
     <base href="<?= $web_root ?>" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="css/styles.css" rel="stylesheet" type="text/css" />
@@ -16,53 +16,43 @@
             <a href="tricount/operations/<?= $tricount->id ?>" class="button" id="back">Back</a>
             <p><?= $tricount->title ?> &#11208; Edit</p>
         </header>
-        <form id="edittricountform" action="tricount/edit_tricount/<?= $tricount->id ?>" method="post">
-            <input type="submit" value="Save" formaction="tricount/edit_tricount/<?= $tricount->id ?>">
-            <table>
-                <h3>Settings</h3>
-                <tr>
-                    <td>Title</td>
-                </tr>
-                <tr>
-                    <td><input id="title" name="title" type="text" size="16" value="<?= $tricount->title ?>"></td>
-                </tr>
-                <tr>
-                    <td>Description (Optional)</td>
-                </tr>
-                <tr>
-                    <td><input id="description" name="description" type="text" size="16" value="<?= $tricount->description ?>"></td>
-                </tr>
-            </table>
+        <h3>Settings</h3>
+        <form id="edittricountform" action="tricount/edit_tricount/<?= $tricount->id ?>" method="post" class="edit">
+            <input type="submit" value="Save" formaction="tricount/edit_tricount/<?= $tricount->id ?>" class="button save">
+            <label>Title :</label>
+            <input id="title" name="title" type="text" value="<?= $tricount->title ?>">
+            <label>Description (optional) :</label>
+            <input id="description" name="description" type="textarea" value="<?= $tricount->description ?>"></td>
         </form>
         <h3>Subscriptions</h3>
-        <table>
+        <table class="subs">
             <tr>
-                <td><?= $creator->full_name ?>(Creator)</td>
+                <td class="subs"><?= $creator->full_name ?> (creator)</td>
             </tr>
             <?php foreach ($subscriptors as $subscriptor) { ?>
-                <tr>
-                    <form class="link" action='tricount/delete_subscriptor/<?= $tricount->id ?>' method='post'>
-                        <input type='text' name='subscriptor_name' value='<?= $subscriptor->id ?>' hidden>
-                        <td>
-                            <p><?= $subscriptor->full_name ?><input type='submit' value='delete'>
-                        </td>
-                        </p>
-                        </td>
-                    </form>
+                <tr class="pop">
+                    <td><?= $subscriptor->full_name ?></td>
+                    <td class="link">
+                        <form class="link" action='tricount/delete_subscriptor/<?= $tricount->id ?>' method='post'>
+                            <input type='text' name='subscriptor_name' value='<?= $subscriptor->id ?>' hidden>
+                            <input type='submit' value='delete' class="pop">
+                        </form>
+                    </td>
                 </tr>
             <?php } ?>
         </table>
         <form id="subscriptor" name="subscriptor" method="POST">
-            
-            <select name="subscriptor" id="subscriptor">
+            <table>
+                <td><select name="subscriptor" id="subscriptor">
                 <option value="">--Add a new subscriber--</option>
                 <?php foreach ($cbo_users as $cbo_user) { ?>
                     <option value="<?= $cbo_user->id ?>"><?= $cbo_user->full_name ?></option>
                 <?php } ?>
-            </select>
-            <input type="submit" value="Add" formaction="tricount/add_subscriptors/<?= $tricount->id ?>">
+            </select></td>
+                <td><input type="submit" value="Add" formaction="tricount/add_subscriptors/<?= $tricount->id ?>"></td>
+            </table>
         </form>
-        <a href="tricount/delete_tricount/<?= $tricount->id ?>">Delete Tricount</a>
+        <a href="tricount/delete_tricount/<?= $tricount->id ?>" class="button bottom delete">Delete this tricount</a>
     </div>
 </body>
 
