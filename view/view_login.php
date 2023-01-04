5809264550a795160c672a3b96917ca690ecbc0d
Cedric Ribaucourt <cedric.ribaucourt@gmail.com>
2023-01-04 16:22:36 +0100

    feat(CSS) : Ajout d'icones sur les champs input lors du login et signUp

--- view/view_login.php
+++ view/view_login.php
@@ -6,6 +6,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<base href="<?= $web_root ?>">
 	<link href="css/styles.css" rel="stylesheet" type="text/css" />
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 	<title>Tricount</title>
 </head>
 
@@ -14,19 +15,25 @@
 		<header class="t1">Tricount</header>
 		<form action="main/login" method="POST" class="connect">
 			<div class="formtitle">Sign In</div>
-			<input class="input" type="text" id="email" name="email" value="<?= $email ?>" <?php if(array_key_exists('empty_email', $errors) || array_key_exists('wrong_email', $errors)) {?>class = "errorInput"<?php } ?>>
-			<?php if (array_key_exists('empty_email', $errors)){ ?>
-                    <p class="errorMessage"><?php echo $errors['empty_email'];?></p>
-                <?php }
-                 if(array_key_exists('wrong_email', $errors)){?>
-                    <p class="errorMessage"><?php echo $errors['wrong_email'];?></p>
-                <?php } ?>
-			<input class="input" type="password" id="password" name="password" value="<?= $password ?>" <?php if(array_key_exists('wrong_password', $errors)) {?>class = "errorInput"<?php } ?>>
-			<?php if (array_key_exists('wrong_password', $errors)){ ?>
-                    <p class="errorMessage"><?php echo $errors['wrong_password'];?></p>
-                <?php } ?>
+			<div class="contains_input">
+				<span class="icon"><i class="fa fa-user" aria-hidden="true"></i></span>
+				<input type="text" id="email" name="email" value="<?= $email ?>" <?php if (array_key_exists('empty_email', $errors) || array_key_exists('wrong_email', $errors)) { ?>class="errorInput" <?php } ?>>
+			</div>
+			<?php if (array_key_exists('empty_email', $errors)) { ?>
+				<p class="errorMessage"><?php echo $errors['empty_email']; ?></p>
+			<?php }
+			if (array_key_exists('wrong_email', $errors)) { ?>
+				<p class="errorMessage"><?php echo $errors['wrong_email']; ?></p>
+			<?php } ?>
+			<div class="contains_input">
+				<span class="icon"><i class="fa fa-lock" aria-hidden="true"></i></span>
+			<input type="password" id="password" name="password" value="<?= $password ?>" <?php if (array_key_exists('wrong_password', $errors)) { ?>class="errorInput" <?php } ?>>
+			</div>
+			<?php if (array_key_exists('wrong_password', $errors)) { ?>
+				<p class="errorMessage"><?php echo $errors['wrong_password']; ?></p>
+			<?php } ?>
 			<input class='login' type="submit" value="Login">
-			<p class = 'join'><a href="main/signup">New here ? Click here to join the party !</a></p>
+			<p class='join'><a href="main/signup">New here ? Click here to join the party !</a></p>
 		</form>
 	</div>
 </body>
