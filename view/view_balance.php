518558c2c3ffe5726bdba774bb5523d7ee05c406
Xavier <2907xaadabunu@student.epfc.eu>
2023-01-10 23:57:35 +0100

    refactor(CSS/HTML): gestion d'erreur via validity de la view_balance

--- view/view_balance.php
+++ view/view_balance.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <base href="<?= $web_root ?>">
-    <link href="css/styles.css" rel="stylesheet" type="text/css" />
+    <link href="css/styles.css" rel="stylesheet" type="text/css">
     <title><?= $tricount->title ?> &#11208; balance</title>
 </head>
 
@@ -14,47 +14,24 @@
         <header class="t2">
             <a href="tricount/operations/<?= $tricount->id ?>" class="button" id="back">Back</a>
             <p><?= $tricount->title ?> &#11208; balance</p>
+            <p></p>
         </header>
         <table class="balance">
             <?php foreach ($subs as $sub) { ?>
                 <tr class="balance">
                     <?php if ($amounts[$sub->id] >= 0) { ?>
-                        <td class="balance"> <p class="left">
-                            <?php if ($sub->id === $user->id) {
-                                echo "<b>";
-                            } ?>
-                            <?= $sub->full_name ?>
-                            <?php if ($sub->id === $user->id) {
-                                echo "(me)</b>";
-                            } ?></p>
+                        <td class="balance">
+                            <p class="left<?php if ($sub->id === $user->id) {echo " bold";} ?>" ><?= $sub->full_name ?><?php if ($sub->id === $user->id) {echo " (me)";} ?></p>
                         </td>
-                        <td class="positive balance"> <p class="positive right" style="width: <?= abs($amounts[$sub->id] / $max * 100) ?>%;">
-                            <?php if ($sub->id === $user->id) {
-                                echo "<b>";
-                            } ?>
-                            <p class="inner"><?= round($amounts[$sub->id], 2) ?> €</p>
-                            <?php if ($sub->id === $user->id) {
-                                echo "</b>";
-                            } ?></p>
+                        <td class="positive balance">
+                            <p class="positive right<?php if ($sub->id === $user->id) {echo " bold";} ?>" style="width: <?= abs($amounts[$sub->id] / $max * 100) ?>%;"><p class="inner" ><?= round($amounts[$sub->id], 2) ?> €</p>
                         </td>
                     <?php } else { ?>
-                        <td class="negative balance"> <p class="negative left" style="width: <?= abs($amounts[$sub->id] / $max * 100) ?>%;">
-                            <?php if ($sub->id === $user->id) {
-                                echo "<b>";
-                            } ?>
-                            <p class="inner left"><?= round($amounts[$sub->id], 2) ?> €</p>
-                            <?php if ($sub->id === $user->id) {
-                                echo "</b>";
-                            } ?></p>
+                        <td class="negative balance">
+                            <p class="negative left" style="width: <?= abs($amounts[$sub->id] / $max * 100) ?>%;"><p class="inner left<?php if ($sub->id === $user->id) {echo " bold";} ?>"><?= round($amounts[$sub->id], 2) ?> €</p>
                         </td>
-                        <td class="balance"> <p class="right">
-                            <?php if ($sub->id === $user->id) {
-                                echo "<b>";
-                            } ?>
-                            <?= $sub->full_name ?>
-                            <?php if ($sub->id === $user->id) {
-                                echo "(me)</b>";
-                            } ?></p>
+                        <td class="balance">
+                            <p class="right<?php if ($sub->id === $user->id) {echo " bold";} ?>"><?= $sub->full_name ?><?php if ($sub->id === $user->id) {echo " (me)";} ?></p>
                         </td>
                     <?php } ?>
                 </tr>
