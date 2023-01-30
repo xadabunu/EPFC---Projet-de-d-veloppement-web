6cbb92581ed4537573fdd01f32b7ab8c3a8d8bec
Cedric <cedric.ribaucourt@gmail.com>
2023-01-30 16:23:31 +0100

    build() : gestion de conflit

--- model/User.php
+++ model/User.php
@@ -126,19 +126,8 @@ class User extends Model
 
     public function persist() : User
     {
-        if(self::get_user_by_email($this->email)){
-<<<<<<< HEAD
-            self::execute("UPDATE users SET hashed_password=:password, full_name=:full_name, role=:role, iban=:iban WHERE mail=:email",
-                            ["password"=>$this->hashed_password, "full_name"=>$this->full_name, "role"=>$this->role, "iban"=>$this->iban]);
-=======
-            self::execute("UPDATE users SET hashed_password=:hashed_password, full_name=:full_name, role=:role, iban=:iban WHERE mail=:email",
-                            ["hashed_password"=>$this->hashed_password, "full_name"=>$this->full_name, "role"=>$this->role, "iban"=>$this->iban]);
->>>>>>> 816a2b555ac43e3279bb49d9b4e1fc502144e468
-        }
-        else{
-            self::execute("INSERT INTO users(mail, hashed_password, full_name, role, iban) VALUES(:email, :password, :full_name, :role, :iban)",
-                            ["email" =>$this->email, "password"=>$this->hashed_password, "full_name"=>$this->full_name,"role"=>$this->role, "iban"=>$this->iban]);
-        }
+        self::execute("INSERT INTO users(mail, hashed_password, full_name, role, iban) VALUES(:email, :password, :full_name, :role, :iban)",
+                            ["email" =>$this->email, "password"=>$this->hashed_password, "full_name"=>$this->full_name,"role"=>$this->role, "iban"=>$this->iban]);        
         $this->id = Model::lastInsertId();
         return $this;
     }
